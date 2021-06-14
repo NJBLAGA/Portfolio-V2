fe0c5141560c736c3cf464a2c2895571d648ae9e
NJBLAGA <nathanblaga90@gmail.com>
2021-06-14 20:44:44 +1000

    Added Project Containers/Styling

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -4,10 +4,16 @@ import {
   ProjectsWrapper,
   Title,
   ProjectContainer,
+  LeftSection,
+  RightSection,
   ProjectImage,
   ProjectName,
   ProjectDescription,
+  ProjectTechStack,
+  TechStackIcon,
+  ProjectLiveDemoButton,
   ProjectLiveDemoLink,
+  ProjectSourceCodeButton,
   ProjectSourceCodeLink,
 } from "../styled-components/Projects";
 
@@ -15,17 +21,39 @@ const Projects = ({ id, title, projects }) => {
   return (
     <>
       <ProjectsContainer id={id}>
+        <Title>{title}</Title>
         <ProjectsWrapper>
-          <Title>{title}</Title>
           {projects.map(function (Project, index) {
-            let { name, image, description, demo, source } = Project;
+            let { name, image, alt, tech_stack, description, demo, source } =
+              Project;
             return (
               <ProjectContainer key={index}>
-                <ProjectImage>{image}</ProjectImage>
-                <ProjectName>{name}</ProjectName>
-                <ProjectDescription>{description}</ProjectDescription>
-                <ProjectLiveDemoLink href="/">{demo}</ProjectLiveDemoLink>
-                <ProjectSourceCodeLink>{source}</ProjectSourceCodeLink>
+                <LeftSection>
+                  <ProjectName>{name}</ProjectName>
+                  <ProjectDescription>{description}</ProjectDescription>
+                  <ProjectTechStack>
+                    {tech_stack.map(function (TechIcon, index) {
+                      return (
+                        <TechStackIcon key={index}>
+                          <TechIcon />
+                        </TechStackIcon>
+                      );
+                    })}
+                  </ProjectTechStack>
+                  <ProjectLiveDemoButton>
+                    <ProjectLiveDemoLink href={demo}>
+                      Live Demo
+                    </ProjectLiveDemoLink>
+                  </ProjectLiveDemoButton>
+                  <ProjectSourceCodeButton>
+                    <ProjectSourceCodeLink href={{ source }}>
+                      Source Code
+                    </ProjectSourceCodeLink>
+                  </ProjectSourceCodeButton>
+                </LeftSection>
+                <RightSection>
+                  <ProjectImage src={image} alt={alt} />
+                </RightSection>
               </ProjectContainer>
             );
           })}
--- src/components/styled-components/Projects.js
+++ src/components/styled-components/Projects.js
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
 export const ProjectsContainer = styled.div`
+  height: 300vh;
   color: black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
   @media screen and (max-width: 768px) {
     padding: 100px 0;
@@ -9,21 +13,20 @@ export const ProjectsContainer = styled.div`
 `;
 
 export const ProjectsWrapper = styled.div`
-  display: grid;
-  z-index: 1;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
   height: 100vh;
   width: 100%;
-  max-width: 1100px;
+  max-width: 1500px;
   margin-left: auto;
   margin-right: auto;
-  padding: 0 24px;
-  justify-content: center;
   background: #fff;
 `;
 
 export const Title = styled.h1`
   margin-top: 50px;
-  margin-bottom: 24px;
+  margin-bottom: 2%;
   font-size: 80px;
   font-weight: 800;
   color: black;
@@ -33,14 +36,101 @@ export const Title = styled.h1`
   }
 `;
 
-export const ProjectContainer = styled.div``;
+export const ProjectContainer = styled.div`
+  width: 80%;
+  height: 450px;
+  margin: 5%;
+  display: flex;
+  flex-direction: row;
+`;
 
-export const ProjectImage = styled.p``;
+export const LeftSection = styled.div`
+  width: 50%;
+  height: 100%;
+  padding-right: 5px;
+`;
 
-export const ProjectName = styled.h1``;
+export const RightSection = styled.div`
+  width: 60%;
+  height: 100%;
+`;
 
-export const ProjectDescription = styled.p``;
+export const ProjectImage = styled.img`
+  width: 100%;
+  height: 70%;
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
 
-export const ProjectLiveDemoLink = styled.p``;
+export const ProjectName = styled.h1`
+  width: 100%;
+  font-size: 40px;
+  margin-bottom: 2%;
+`;
 
-export const ProjectSourceCodeLink = styled.p``;
+export const ProjectDescription = styled.p`
+  width: 100%;
+  width: 90%;
+  margin-bottom: 2%;
+`;
+
+export const ProjectTechStack = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 2%;
+`;
+
+export const TechStackIcon = styled.p`
+  font-size: 50px;
+  margin: 5px;
+  &:hover {
+    transform: scale(1.3);
+    color: #f64f59;
+  }
+`;
+
+export const ProjectLiveDemoButton = styled.button`
+  display: inline;
+  width: 100px;
+  height: 50px;
+  border: 1.5px solid #232a34;
+  background-color: #232a34;
+  cursor: pointer;
+  transition: all 0.4s;
+  z-index: 3;
+
+  &:hover {
+    color: #fff;
+    border: none;
+    background: linear-gradient(to right, #f64f59, #c471ed, #12c2e9);
+  }
+`;
+
+export const ProjectLiveDemoLink = styled.a`
+  text-decoration: none;
+  color: #fff;
+`;
+
+export const ProjectSourceCodeButton = styled.button`
+  display: inline;
+  width: 100px;
+  height: 50px;
+  border: 1.5px solid #232a34;
+  background-color: #232a34;
+  cursor: pointer;
+  transition: all 0.4s;
+  z-index: 3;
+  margin: 5px;
+
+  &:hover {
+    color: #fff;
+    border-color: #232a34;
+    background: linear-gradient(to right, #f64f59, #c471ed, #12c2e9);
+  }
+`;
+
+export const ProjectSourceCodeLink = styled.a`
+  text-decoration: none;
+  color: #fff;
+`;
