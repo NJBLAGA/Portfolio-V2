dc6c0b33ba1aa1e3a49efaa905eba379d11ba417
NJBLAGA <nathanblaga90@gmail.com>
2021-06-15 22:11:33 +1000

    Added Details/Spelling

--- src/components/Sections/pages.js
+++ src/components/Sections/pages.js
@@ -34,7 +34,7 @@ export const aboutSection = {
   id: "about",
   title: "About Me",
   bioOne:
-    "I am a creative, driven and passionate software developer, currently studying at Coder Academy Sydney. I am highly motivated to learn, adapt and seek growth in my personal and professional life. I work efficiently and effectively as an individual and within team collaborations. I am determined to build a strong network of like-minded developers who wish to improve and grow within the industry. In my spare time I love Powerlifting, Hiking and Video Games.",
+    "I am a creative, driven and passionate software developer, currently studying at Coder Academy Sydney. I am highly motivated to learn, adapt and seek growth in my personal and professional life. I work efficiently and effectively as an individual and within team collaborations. I am determined to build a strong network of like-minded developers who wish to improve and grow within the industry. In my spare time, I love powerlifting, hiking and playing video games.",
   img: nathan,
   alt: "Picture of Me",
   skills: [
@@ -79,7 +79,7 @@ export const projectsSection = {
         SiTrello,
       ],
       description:
-        "My second Coder Academy hackathon project utilised newsapi.org and React to build a one page application that offers it's users news articles from across the globe. The project challenged us to develop an adaptive and user-friendly application utilising React states.",
+        "For my second Coder Academy hackathon, my team utilised newsapi.org and React to build a one page application that offers users news articles from across the globe. The project challenged us to develop an adaptive and user-friendly application which utilised React states.",
       demo: "https://github.com/Channel7News/reackathon",
       source: "https://github.com/Channel7News/reackathon",
     },
@@ -90,7 +90,7 @@ export const projectsSection = {
       alt: "Image of Get Lucky 3000 Landing Page",
       tech_stack: [SiHtml5, SiCss3, FaJs, SiGithub, SiTrello, SiNetlify],
       description:
-        "As my first Coder Academy hackathon project, I developed a web application that generated a dating profile based on the information provided by the user. Working within a team challenged my communication skills and required a professional approach to the team's source control practices. Utilising several APIs within the project also posed challenging obstacles which we were required to overcome.  ",
+        "As my first Coder Academy hackathon project, my team developed a web application that generated a dating profile based on the information provided by the user. Working within a team challenged my communication skills and required a professional approach to the team's source control practices. Utilising several APIs within the project also posed obstacles which we were required to overcome.  ",
       demo: "https://getlucky3000.netlify.app/",
       source: "https://github.com/theandrewfulton/getlucky3000",
     },
@@ -121,7 +121,7 @@ export const projectsSection = {
       alt: "Image of the Main Menu of Zop",
       tech_stack: [SiRuby, SiGithub, SiTrello],
       description:
-        "A terminal based quiz game built completely using Ruby. Zop prompts a player with a series of random questions ranging in difficulty while tracking their current score and remaining attempts. This project challenged me to focus on test driven development and error handling. ",
+        "A terminal based quiz game built completely using Ruby. Zop prompts a player with a series of random questions ranging in difficulty, while tracking their current score and remaining attempts. This project challenged me to focus on test driven development and error handling. ",
       demo: "https://github.com/NJBLAGA/NathanBlaga_T1A3",
       source: "https://github.com/NJBLAGA/NathanBlaga_T1A3",
     },
@@ -139,7 +139,7 @@ export const projectsSection = {
         SiNetlify,
       ],
       description:
-        "The first iteration of my personal portfolio. It's purpose was to create a professional platform in which I could demonstrate my abilities to future employers.The project provided the audience with the current state of my talents, abilities and coding experience, alongside an insight into my personality, professional knowledge, interests, hobbies and mindset.",
+        "The purpose of the first iteration of my personal portfolio was to create a professional platform in which I could demonstrate my abilities to future employers. The project provided the audience with  my talents, abilities and coding experience during that time. It also provided an insight into my personality, professional knowledge, interests, hobbies and mindset.",
       demo: "https://nathanblaga.netlify.app/",
       source: "https://github.com/NJBLAGA/Personal_Portfolio",
     },
@@ -150,7 +150,7 @@ export const contactSection = {
   id: "contact",
   title: "Contact",
   message:
-    "If you wish to get in touch please reach out to me by email or on social media.",
+    "If you wish to get in touch, please reach out to me via email or on social media.",
   socialMedia: [
     {
       icon: FaFileDownload,
